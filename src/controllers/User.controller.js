d52f0331086cb59686e936ae94aaa05e76f89a18
Shubham <shubhamkumar7880@gmail.com>
2024-08-23 17:06:51 +0530

    refresh token  removed after logout

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -122,8 +122,8 @@ const loginUser = asyncHandler(async (req, res) => {
 const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(req.user._id,
         {
-            $set: {      // $set is a mongodb operator that sets a value to the field.
-                refreshToken: undefined,
+            $unset: {      // $set is a mongodb operator that sets a value to the field.
+                refreshToken: "",
             }
         },
         {
