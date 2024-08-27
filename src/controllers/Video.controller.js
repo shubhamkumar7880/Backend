3e24402c74dabe6f7c8d5d09fc715650dbfcabb7
Shubham <shubhamkumar7880@gmail.com>
2024-08-27 15:22:46 +0530

    added video endpoints

--- src/controllers/Video.controller.js
+++ src/controllers/Video.controller.js
@@ -1,4 +1,3 @@
-import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/Video.model.js";
 import { User } from "../models/User.model.js";
 import ApiError from "../utils/ApiError.js";
@@ -18,29 +17,29 @@ const getAllVideos = asyncHandler(async (req, res) => {
     }
     const skip = (page - 1) * limit;
 
-   try {
-     const videos = await Video.find({ owner: user._id, isPublished: true })
-         .sort({ [sortBy]: sortType === 'desc' ? -1 : 1 })  // Sorting based on the query parameters
-         .skip(skip)  // Skipping documents for pagination
-         .limit(parseInt(limit));  // Limiting the number of documents
- 
-     if (!videos) {
-         throw new ApiError(404, "No videos found");
-     }
-     const totalVideos = await Video.countDocuments({ owner: user._id, isPublished: true });
- 
-     return res.status(200).json(
-         new ApiResponse(200, {
-             page: parseInt(page),
-             limit: parseInt(limit),
-             totalPages: Math.ceil(totalVideos / limit),
-             totalVideos,
-             videos
-         }, "Videos fetched successfully")
-     );
-   } catch (error) {
-    throw new ApiError(500, error?.message || "Something went wrong!");
-   }
+    try {
+        const videos = await Video.find({ owner: user._id, isPublished: true })
+            .sort({ [sortBy]: sortType === 'desc' ? -1 : 1 })  // Sorting based on the query parameters
+            .skip(skip)  // Skipping documents for pagination
+            .limit(parseInt(limit));  // Limiting the number of documents
+
+        if (!videos) {
+            throw new ApiError(404, "No videos found");
+        }
+        const totalVideos = await Video.countDocuments({ owner: user._id, isPublished: true });
+
+        return res.status(200).json(
+            new ApiResponse(200, {
+                page: parseInt(page),
+                limit: parseInt(limit),
+                totalPages: Math.ceil(totalVideos / limit),
+                totalVideos,
+                videos
+            }, "Videos fetched successfully")
+        );
+    } catch (error) {
+        throw new ApiError(500, error?.message || "failed to fetch videos!");
+    }
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -84,42 +83,100 @@ const publishAVideo = asyncHandler(async (req, res) => {
             new ApiResponse(201, videoModel, "Video created successfully")
         );
     } catch (error) {
-        throw new ApiError(500, error?.message || "Something went wrong!");
+        throw new ApiError(500, error?.message || "Failed to publish video!");
     }
 
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
-    if(!videoId){
+    if (!videoId) {
         throw new ApiError(400, "Video ID is required");
     }
     try {
         const video = await Video.findById(videoId);
-        if(!video){
+        if (!video) {
             throw new ApiError(404, "Video not found");
         }
         return res.status(201).json(
             new ApiResponse(201, video, "Video fetched successfully!")
         );
     } catch (error) {
-        throw new ApiError(500, error?.message || "Something went wrong!");
+        throw new ApiError(500, error?.message || "failed to fetch video!");
     }
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
+    let video;
     const { videoId } = req.params;
-    //TODO: update video details like title, description, thumbnail
+    const { title, description } = req.body;
 
+    if (!(title && description)) {
+        throw new ApiError(400, "Title and description are required");
+    }
+    if (!videoId) {
+        throw new ApiError(400, "Video ID is required");
+    }
+    try {
+        const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+        if (thumbnailLocalPath) {
+            const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+            if (!thumbnail) {
+                throw new ApiError(400, "Failed to upload thumbnail");
+            }
+            video = await Video.findByIdAndUpdate(videoId, { $set: { title, description, thumbnail: thumbnail?.url } }, { new: true });
+        } else {
+            video = await Video.findByIdAndUpdate(videoId, { $set: { title, description } }, { new: true });
+        }
+        if (!video) {
+            throw new ApiError(500, "Failed to update video deatils");
+        }
+
+        return res.status(201).json(
+            new ApiResponse(201, video, "Video details updated successfully!")
+        );
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Failed to update video deatils!");
+    }
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
-    //TODO: delete video
+    try {
+        const deletedVideo = await Video.findOneAndDelete({ _id: videoId });
+
+        if (!deletedVideo) {
+            throw new ApiError(500, "Video not found!");
+        }
+
+        return res.status(201).json(
+            new ApiResponse(201, deletedVideo, "Video deleted successfully!")
+        );
+    } catch (err) {
+        throw new ApiError(500, err?.message || "Error deleting video!");
+    }
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
+    const {isPublished } = req.body;
+    if (!videoId) {
+        throw new ApiError(400, "Video ID is required");
+    }
+   try {
+     const video = await Video.findByIdAndUpdate(videoId, { $set: { isPublished } }, { new: true });
+ 
+     if (!video) {
+         throw new ApiError(500, "Failed to toggle publish status");
+     }
+ 
+     return res.status(201).json(
+         new ApiResponse(201, video, "Toggle publish status successfully!")
+     );
+   } catch (error) {
+    throw new ApiError(500, err?.message || "Error toggling publish status!");
+   }
+
 });
 
 export {
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import ApiError from "../utils/ApiError.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-        const token = req.cookies?.accessToken || require.header("Authorization")?.replace("Bearer ", "");
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
     
         if(!token) {
             throw new ApiError(401, "Unauthorized request");
