3f54fdf0f1afaff1bd65ab5a20e52ed1edbaf475
Subham Kumar <shubhamkumar7880@gmail.com>
2025-08-06 15:47:41 +0530

    added tweet comments controller

--- src/controllers/Comment.controller.js
+++ src/controllers/Comment.controller.js
@@ -4,6 +4,7 @@ import ApiError from '../utils/ApiError.js';
 import ApiResponse from '../utils/ApiResponse.js';
 import asyncHandler from '../utils/asyncHandler.js';
 import { Video } from '../models/Video.model.js';
+import { Tweet } from '../models/Tweet.model.js';
 
 const getVideoComments = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
@@ -72,6 +73,73 @@ const getVideoComments = asyncHandler(async (req, res) => {
   );
 });
 
+const getTweetComments = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const {
+    page = 1,
+    limit = 10,
+    sortBy = 'createdAt',
+    sortType = 'desc',
+  } = req.query;
+  if (!tweetId || !isValidObjectId(tweetId)) {
+    throw new ApiError(400, 'Invalid video ID');
+  }
+  const skip = (page - 1) * limit;
+  const comments = await Comment.aggregate([
+    {
+      $match: { tweet: new mongoose.Types.ObjectId(tweetId) },
+    },
+    {
+      $sort: { [sortBy]: sortType === 'desc' ? -1 : 1 },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: parseInt(limit),
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'owner',
+        foreignField: '_id',
+        as: 'owner',
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: '$owner',
+    },
+  ]);
+  if (!comments || comments.length === 0) {
+    throw new ApiError(404, 'No Comments found');
+  }
+  const totalComments = await Comment.countDocuments({
+    tweet: tweetId,
+  });
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        page: parseInt(page),
+        limit: parseInt(limit),
+        totalPages: Math.ceil(totalComments / limit),
+        totalComments,
+        comments,
+      },
+      'Comments fetched successfully',
+    ),
+  );
+});
+
 const addComment = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   const { content } = req.body;
@@ -83,9 +151,39 @@ const addComment = asyncHandler(async (req, res) => {
     throw new ApiError(400, 'Invalid video ID');
   }
 
+  const videoDetail = await Video.findById(videoId);
+  if (!videoDetail) {
+    throw new ApiError(400, 'Invalid Video ID');
+  }
+
   const comment = await Comment.create({
     content,
     video: videoId,
+    owner: req.user._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, comment, 'Comment added successfully'));
+});
+
+const addTweetComment = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, 'Content is required');
+  }
+  if (!tweetId || !isValidObjectId(tweetId)) {
+    throw new ApiError(400, 'Invalid tweet ID');
+  }
+  const tweetDetail = await Tweet.findById(tweetId);
+  if (!tweetDetail) {
+    throw new ApiError(400, 'Invalid Tweet ID');
+  }
+  const comment = await Comment.create({
+    content,
+    tweet: tweetId,
     owner: req.user._id,
   });
 
@@ -150,5 +248,40 @@ const deleteComment = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, null, 'Comment deleted successfully'));
 });
+const deleteTweetComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
 
-export { getVideoComments, addComment, updateComment, deleteComment };
+  if (!commentId || !isValidObjectId(commentId)) {
+    throw new ApiError(400, 'Invalid comment ID');
+  }
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, 'Comment not found');
+  }
+
+  // Check if the current user is the owner
+  if (comment.owner.toString() === req.user._id.toString()) {
+    await comment.deleteOne();
+  } else {
+    const tweet = await Tweet.findById(comment.tweet);
+    if (tweet?.owner.toString === req.user._id.toString()) {
+      await comment.deleteOne();
+    } else {
+      throw new ApiError(403, 'You are not authorized to delete this comment');
+    }
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, 'Comment deleted successfully'));
+});
+export {
+  getVideoComments,
+  addComment,
+  updateComment,
+  deleteComment,
+  addTweetComment,
+  getTweetComments,
+  deleteTweetComment,
+};
--- src/routes/Comment.route.js
+++ src/routes/Comment.route.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import {
   addComment,
+  addTweetComment,
   deleteComment,
+  deleteTweetComment,
+  getTweetComments,
   getVideoComments,
   updateComment,
 } from '../controllers/Comment.controller.js';
@@ -12,6 +15,9 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route('/:videoId').get(getVideoComments).post(addComment);
+
+router.route('/tweet/:tweetId').post(addTweetComment).get(getTweetComments);
 router.route('/c/:commentId').delete(deleteComment).patch(updateComment);
+router.route('/tweet/c/:commentId').delete(deleteTweetComment);
 
 export default router;
