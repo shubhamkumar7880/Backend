090204d2de313b7be573f6c8c621e521b6877252
Subham Kumar <shubhamkumar7880@gmail.com>
2025-08-06 14:45:36 +0530

    added commnets controller

---
+++ src/controllers/Comment.controller.js
@@ -0,0 +1,154 @@
+import mongoose, { isValidObjectId } from 'mongoose';
+import { Comment } from '../models/Comment.model.js';
+import ApiError from '../utils/ApiError.js';
+import ApiResponse from '../utils/ApiResponse.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import { Video } from '../models/Video.model.js';
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const {
+    page = 1,
+    limit = 10,
+    sortBy = 'createdAt',
+    sortType = 'desc',
+  } = req.query;
+  if (!videoId || !isValidObjectId(videoId)) {
+    throw new ApiError(400, 'Invalid video ID');
+  }
+  const skip = (page - 1) * limit;
+  const comments = await Comment.aggregate([
+    {
+      $match: { video: new mongoose.Types.ObjectId(videoId) },
+    },
+    {
+      $sort: { [sortBy]: sortType === 'desc' ? -1 : 1 },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: parseInt(limit),
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'owner',
+        foreignField: '_id',
+        as: 'owner',
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: '$owner',
+    },
+  ]);
+  if (!comments || comments.length === 0) {
+    throw new ApiError(404, 'No Comments found');
+  }
+  const totalComments = await Comment.countDocuments({
+    video: videoId,
+  });
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        page: parseInt(page),
+        limit: parseInt(limit),
+        totalPages: Math.ceil(totalComments / limit),
+        totalComments,
+        comments,
+      },
+      'Comments fetched successfully',
+    ),
+  );
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, 'Content is required');
+  }
+  if (!videoId || !isValidObjectId(videoId)) {
+    throw new ApiError(400, 'Invalid video ID');
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, comment, 'Comment added successfully'));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+  if (!content) {
+    throw new ApiError(400, 'Content is required');
+  }
+  if (!commentId || !isValidObjectId(commentId)) {
+    throw new ApiError(400, 'Invalid comment ID');
+  }
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, 'Comment not found');
+  }
+
+  // Check if the current user is the owner
+  if (comment.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, 'You are not authorized to update this comment');
+  }
+
+  // Now update the comment
+  comment.content = content;
+  await comment.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, 'Comment updated successfully'));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+
+  if (!commentId || !isValidObjectId(commentId)) {
+    throw new ApiError(400, 'Invalid comment ID');
+  }
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, 'Comment not found');
+  }
+
+  // Check if the current user is the owner
+  if (comment.owner.toString() === req.user._id.toString()) {
+    await comment.deleteOne();
+  } else {
+    const video = await Video.findById(comment.video);
+    if (video?.owner.toString === req.user._id.toString()) {
+      await comment.deleteOne();
+    } else {
+      throw new ApiError(403, 'You are not authorized to delete this comment');
+    }
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, 'Comment deleted successfully'));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
--- src/models/Comment.model.js
+++ src/models/Comment.model.js
@@ -1,26 +1,30 @@
-import mongoose, {Schema} from "mongoose";
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import mongoose, { Schema } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
 const commentSchema = new Schema(
-    {
-        content: {
-            type: String,
-            required: true
-        },
-        video: {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        }
-    }, 
-    {
-        timestamps: true,
-    }
-    );
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: 'Video',
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: 'Tweet',
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-    commentSchema.plugin(mongooseAggregatePaginate);
+commentSchema.plugin(mongooseAggregatePaginate);
 
-export const Comment = mongoose.model("Comment", commentSchema);
+export const Comment = mongoose.model('Comment', commentSchema);
